e3aa391bea27b571086eda7a667be3c914c4b1ab
Beata.Kusza <anja37@o2.pl>
2017-03-26 16:47:00 +0200

    add notemon as dev dep; add simple form; create post on subm

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+console.log('Zmiana');
+
 app.listen(port, () => {
   console.log(`Serwer is up and running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-  console.log(_dirname);
+  console.log(__dirname);
   res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/titles', (req, res) => {
+  console.log('Wait for titles');
 });
