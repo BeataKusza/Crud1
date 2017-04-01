d50a37c82d4751377a588ff1231ea18a47a72730
Beata.Kusza <anja37@o2.pl>
2017-04-01 10:58:33 +0200

    add book schema

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyPareser = require('body-parser');
-const MongoClient = reqiure('mogodb').MongoClient;
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 3000;
@@ -9,17 +9,31 @@ let db;
 console.log('Zmiana');
 
 app.use(bodyParser.urlencoded({encode: true, extended: true}));
-db = res;
-app.listen(port, () => {
-  console.log(`Serwer is up and running on port ${port}`);
+
+
+MongoClient.connect('mongodb://test123:123qwe@ds147520.mlab.com:47520/mybase', (err,res)=> {
+  if(err) {
+    return console.error(err)
+  }
+  db = res;
+  app.listen(port, () => {
+    console.log(`Serwer is up and running on port ${port}`);
+  });
 });
 
-MongoClient.connect('mongodb://beata23236:beatakusza1@ds147520.mlab.com:47520/mybase', (err,res)=> (
 app.get('/', (req, res) => {
   console.log(__dirname);
   res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/titles', (req, res) => {
-  console.log('Wait for titles');
+  db.collections('book').save(req.body, (err, result) => {
+    console.log(req.body);
+    if(err) {
+      return console.error(err)
+    }
+
+    console.log.info('Saved to database');
+    res.rederict('/');
+  })
 });
