95976a9036ccbfac7923281be703d0d3af6d7798
Beata.Kusza <anja37@o2.pl>
2017-04-01 09:46:51 +0200

    mongodb

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyPareser = require('body-parser');
+const MongoClient = reqiure('mogodb').MongoClient;
+
 const app = express();
 const port = 3000;
+let db;
 
 console.log('Zmiana');
 
+app.use(bodyParser.urlencoded({encode: true, extended: true}));
+db = res;
 app.listen(port, () => {
   console.log(`Serwer is up and running on port ${port}`);
 });
 
+MongoClient.connect('mongodb://beata23236:beatakusza1@ds147520.mlab.com:47520/mybase', (err,res)=> (
 app.get('/', (req, res) => {
   console.log(__dirname);
   res.sendFile(__dirname + '/index.html')
